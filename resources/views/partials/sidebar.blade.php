a43c854fdc3f4cf75aff5e67919b8379be83d1ef
Francesco Rocco <francescorocco97@gmail.com>
2023-05-22 17:19:06 +0200

    first commit

---
+++ resources/views/partials/sidebar.blade.php
@@ -0,0 +1,14 @@
+<div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary">
+    <a href="/" class="mb-3 mb-md-0 me-md-auto text-decoration-none">
+      <span class="fs-3">Boolfolio</span>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="{{route('admin.dashboard')}}" class="nav-link active" aria-current="page">
+          <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
+            Dashboard
+        </a>
+      </li>
+    </ul> 
+</div>
